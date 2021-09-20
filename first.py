a897a2f487a929186492451017739749a5a495d0
pavansw <pavanwankhade4u@gmail.com>
2021-09-20 17:04:56 +0530

    Updating first.py with graphical way from GitHub

--- first.py
+++ first.py
@@ -1,3 +1,6 @@
 print ("This is my first code I am sharing")
 print ("This is my first code I am sharing")
 print ("This is my first code I am sharing")
+print ("This is my first code I am sharing")
+print ("This is my first code I am sharing")
+print ("This is my first code I am sharing"
